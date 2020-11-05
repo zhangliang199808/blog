f9f8b1dc80018e0b703391e29f8dfcdfb03d5406
张亮 <2034118975@qq.com>
2020-11-05 18:28:36 +0800

    论坛博客

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <div id="app" class="app clearfix">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>
-        <router-view :key="key"></router-view>
-      </el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
+    <input type="hidden" id="pdLogin" value="sdd"/>
+    <router-view :key="key"></router-view>
   </div>
 </template>
 
 <script>
+// import statistics from 'http://static.snail.com/js/stone/v2/statistics_ty_v2.source.js'
 export default {
   name: 'app',
   computed: {
@@ -19,8 +15,28 @@ export default {
     }
   },
   mounted () {
+    const s = document.createElement('script')
+    s.type = 'text/javascript'
+    s.src = 'http://static.snail.com/js/stone/v2/statistics_ty_v2.source.js'
+    document.body.appendChild(s)
   },
-  created () {
+  created: function () {
+    var vm = this
+    vm.$http({
+      url: '//moment.snail.com/api/v1/user/info',
+      method: 'jsonp',
+      jsonp: 'callback',
+      emulateJSON: true,
+      headers: {
+        'Content-Type': 'x-www-from-urlencoded'
+      }
+    }).then(function (res) {
+      if (res.data.code === 200) {
+        document.getElementById('pdLogin').value = 'true'
+      } else {
+        document.getElementById('pdLogin').value = 'false'
+      }
+    })
   }
 }
 </script>
